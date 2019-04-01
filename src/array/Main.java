aaaf119347462731539a510380eea8e9ecdc1563
1ncounter <yj7126@yeah.net>
2019-04-01 17:41:17 +0800

    add plusOne, findDiagonalOrder

--- src/array/Main.java
+++ src/array/Main.java
@@ -1,14 +1,17 @@
 package array;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,7,3,6,5,6};
+        int[] nums = {9};
 
-        int s = pivotIndex(nums);
-        System.out.println(s);
+        int[] s = plusOne(nums);
+        System.out.println(Arrays.toString(s));
     }
 
     /**
@@ -57,7 +60,81 @@ public class Main {
         return max1 > 2 * max2 ? maxIndex : -1;
     }
 
-    public int[] plusOne(int[] digits) {
+    /**
+     * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+     * @param digits int[]
+     * @return int[]
+     */
+    public static int[] plusOne(int[] digits) {
+        if (digits.length == 0) return digits;
 
+        int carry = 1;
+        int len = digits.length;
+
+        for (int i = len - 1; i >= 0; i--) {
+            if (carry == 0) return digits;
+
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10;
+            carry = sum / 10;
+        }
+
+        int[] res = new int[len + 1];
+        res[0] = 1;
+
+        return carry == 0 ? digits : res;
+    }
+
+    /**
+     * 对角线遍历
+     * @param matrix 二位数组
+     * @return 一维数组
+     */
+    public static int[] findDiagonalOrder(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return new int[0];
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] order = new int[m * n];
+        int row = 0;
+        int col = 0;
+        int d = 0;
+        int[][] dirs = {{-1, 1}, {1, -1}};
+
+        for (int i = 0; i < m * n; i++) {
+            order[i] = matrix[row][col];
+            row += dirs[d][0];
+            col += dirs[d][1];
+
+            if (row >= m) {
+                row = m - 1;
+                col += 2;
+                d = 1 - d;
+            }
+            if (col >= n) {
+                col = n - 1;
+                row += 2;
+                d = 1 - d;
+            }
+            if (row < 0) {
+                row = 0;
+                d = 1 - d;
+            }
+            if (col < 0) {
+                col = 0;
+                d = 1 - d;
+            }
+        }
+
+        return order;
+    }
+
+    /**
+     * 给定一个包含 m x n 个元素的矩阵（m 行, n 列），请按照顺时针螺旋顺序，返回矩阵中的所有元素。
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        return result;
     }
 }
